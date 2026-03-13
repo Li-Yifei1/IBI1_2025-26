3c742a786201d377435639b2baf81191e192a73a
Li-Yifei1 <yifei.25@intl.zju.edu.cn>
2026-03-13 12:52:34 +0800

    Delete Practical 4/infection.py

--- Practical 4/infection.py
+++
@@ -1,9 +0,0 @@
-#initial infector
-a=5 
-#days of infection since start
-d=1 
-while a<91:   #the number of total students is 91, once the infected person get 91 or more, the loop stop 
-    a=a*1.4 #the number of infected students the growth rate is 40%
-    d=d+1   
-    print(d,a) #print current day and its number of infected person
-print ("all the students are infected after",d,"days") #print final result
