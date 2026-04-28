f5b8b4d33b2173d4fd087b43ee9c538235779078
Li-Yifei1 <yifei.25@intl.zju.edu.cn>
2026-04-28 22:25:33 +0800

    Update stop_codons.py

--- Practical 7/stop_codons.py
+++ Practical 7/stop_codons.py
@@ -1,59 +1,34 @@
-START_CODON_DNA = 'ATG'
-STOP_CODONS_DNA = {'TAA', 'TAG', 'TGA'}
-INPUT_FILE = r'C:\Users\thinkpad\Downloads\Practical 7\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
-OUTPUT_FILE = r'C:\Users\thinkpad\Downloads\Practical 7\stop_genes.fa'
+import re
 
-current_header = ''
-current_seq = ''
+input_file = "Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
+output_file = "stop_genes.fa"
+stop_pattern = "(TAA|TAG|TGA)"
 
-with open(INPUT_FILE, 'r') as infile, open(OUTPUT_FILE, 'w') as outfile:
-    outfile.write("# This file is created automatically\n")
+with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+    gene_name = ""
+    gene_seq = ""
 
-    for line in infile:
-        line = line.strip()
-        if not line:
-            continue
-        if line.startswith('>'):
-            if current_header and current_seq:
-                start_index = current_seq.find(START_CODON_DNA)
-                found_stop_codons = set()
-                if start_index != -1:
-                    for j in range(start_index + 3, len(current_seq) - 2, 3):
-                        codon = current_seq[j:j+3]
-                        if codon in STOP_CODONS_DNA:
-                            found_stop_codons.add(codon)
-                if found_stop_codons:
-                    gene_name = ''
-                    for part in current_header.split():
-                        if part.startswith('gene:'):
-                            gene_name = part.split(':')[1]
-                            break
-                    new_header = f'>{gene_name} {" ".join(sorted(found_stop_codons))}'
-                    outfile.write(new_header + '\n')
-                    outfile.write(current_seq + '\n')
-
-            current_header = line
-            current_seq = ''
+    for line in f_in:
+        line = line.rstrip()
+        if line.startswith(">"):
+            # Process the previous gene using Lecture logic
+            if gene_seq.startswith("ATG"):
+                # Use regex to find all in-frame stop codons
+                found_stops = []
+                for i in range(0, len(gene_seq)-2, 3):
+                    codon = gene_seq[i:i+3]
+                    match = re.search(stop_pattern, codon)
+                    if match:
+                        found_stops.append(match.group())
+                
+                if found_stops:
+                    unique_stops = ",".join(set(found_stops))
+                    f_out.write(f">{gene_name};{unique_stops}\n{gene_seq}\n")
+            
+            name_match = re.search(r'^>(\S+)', line)
+            gene_name = name_match.group(1) if name_match else "Unknown"
+            gene_seq = ""
         else:
-            current_seq += line
+            gene_seq += line
 
-    if current_header and current_seq:
-        start_index = current_seq.find(START_CODON_DNA)
-        found_stop_codons = set()
-        if start_index != -1:
-            for j in range(start_index + 3, len(current_seq)-2, 3):
-                codon = current_seq[j:j+3]
-                if codon in STOP_CODONS_DNA:
-                    found_stop_codons.add(codon)
-        if found_stop_codons:
-            gene_name = ''
-            for part in current_header.split():
-                if part.startswith('gene:'):
-                    gene_name = part.split(':')[1]
-                    break
-            new_header = f'>{gene_name} {" ".join(sorted(found_stop_codons))}'
-            outfile.write(new_header + '\n')
-            outfile.write(current_seq + '\n')
-
-print("Done! The output file is definitely created in your folder.")
-
+# Final gene processing logic (same as inside the loop)
