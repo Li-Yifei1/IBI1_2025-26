757b3de54787282d212ef016e11bc1a6a88a71f5
Li-Yifei1 <yifei.25@intl.zju.edu.cn>
2026-05-11 21:31:41 +0800

    Add files via upload

---
+++ Practical 8/predicting_protein_mass.py
@@ -0,0 +1,18 @@
+amino_mass = {
+    'G': 57.02, 'A': 71.04, 'S': 87.03, 'P': 97.05, 'V': 99.07,
+    'T': 101.05, 'C': 103.01, 'I': 113.08, 'L': 113.08, 'N': 114.04,
+    'D': 115.03, 'Q': 128.06, 'K': 128.09, 'E': 129.04, 'M': 131.04,
+    'H': 137.06, 'F': 147.07, 'R': 156.10, 'Y': 163.06, 'W': 186.08
+}
+def calculate_protein_mass(aa_sequence):
+    total_mass = 0.0
+    cleaned_sequence = aa_sequence.replace(" ", "").strip().upper()
+    for aa in cleaned_sequence:
+        if aa not in amino_mass:
+            raise ValueError(f"Error: Invalid amino acid '{aa}' found in sequence.")
+        total_mass += amino_mass[aa]
+    return total_mass
+
+# test 1
+aa_sequence = "GACT"
+print("the total mass of ", aa_sequence, "is ", calculate_protein_mass(aa_sequence))
